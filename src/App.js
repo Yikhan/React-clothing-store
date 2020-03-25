d5c913a0f246002939d30e1ec44f3335d82a8cf7
Yikhan <yhxiaoch@gmail.com>
2020-03-25 23:01:07 +1100

    add react-router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
-import './App.css'
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component'
-import React from 'react'
+import { Route, Switch } from "react-router-dom";
+
+import HomePage from "./pages/homepage/homepage.component";
+import React from "react";
+
+const HatsPage = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>HATS PAGE</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/hats" component={HatsPage} />
+      </Switch>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import './index.css';
+import "./index.css";
 
-import App from './App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </BrowserRouter>,
+  document.getElementById("root")
 );
-
