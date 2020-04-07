f44ed84c61ae3cf1dd44ed44fbf5c353ec3e40d6
Yikhan <yhxiaoch@gmail.com>
2020-04-07 22:40:35 +1000

    implement header

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import "./App.css";
+import './App.css';
 
-import { Route, Switch } from "react-router-dom";
+import { Route, Switch } from 'react-router-dom';
 
-import HomePage from "./pages/homepage/homepage.component";
-import React from "react";
-import ShopPage from "./pages/shop/shop.component"
+import Header from './components/header/header.component'
+import HomePage from './pages/homepage/homepage.component';
+import React from 'react';
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/shop" component={ShopPage} />
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
