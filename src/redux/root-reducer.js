356b381476ee6a203f5074255822eab6e69ae0db
Yikhan <yhxiaoch@gmail.com>
2020-04-29 20:35:09 +1000

    add shop data to redux

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -2,6 +2,7 @@ import cartReducer from './cart/cart.reducer';
 import { combineReducers } from 'redux';
 import directoryReducer from './directory/directory.reducer';
 import { persistReducer } from 'redux-persist';
+import shopReducer from './shop/shop.reducer';
 import storage from 'redux-persist/lib/storage';
 import userReducer from './user/user.reducer';
 
@@ -15,7 +16,8 @@ const persistConfig = {
 const rootReducer = combineReducers({
 	user: userReducer,
 	cart: cartReducer,
-	directory: directoryReducer
+  directory: directoryReducer,
+  shop: shopReducer
 });
 
 //* NOTE 持久化储存redux数据
