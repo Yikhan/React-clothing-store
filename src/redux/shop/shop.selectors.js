a4ad86259173a4387102cee70aff97f7c70edde1
Yikhan <yhxiaoch@gmail.com>
2020-05-01 10:34:55 +1000

    update selector for collection overview

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -4,5 +4,9 @@ const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector([ selectShop ], (shop) => shop.collections);
 
+export const selectCollectionsForOverview = createSelector([ selectCollections ], (collections) =>
+	Object.values(collections)
+);
+
 export const selectCollection = (collectionName) =>
 	createSelector([ selectCollections ], (collections) => collections[collectionName]);
