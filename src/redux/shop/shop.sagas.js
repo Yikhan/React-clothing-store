fb32d2f80c229c245e15757fd090e5fcb5ec74d1
Yikhan <yhxiaoch@gmail.com>
2020-06-02 23:27:28 +1000

    create root saga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { call, put, takeEvery } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
 import { convertCollectionsSnapshotToMap, firestore } from '../../firebase/firebase.utils';
 import { fetchCollectionsFailure, fetchCollectionsSuccess } from './shop.actions';
 
@@ -20,5 +20,5 @@ export function* fetchCollectionsAsync() {
 }
 
 export function* fetchCollectionsStart() {
-	yield takeEvery(ShopActionTypes.FETCH_COLLECTION_START, fetchCollectionsAsync);
+	yield takeLatest(ShopActionTypes.FETCH_COLLECTION_START, fetchCollectionsAsync);
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,10 @@
 import { applyMiddleware, createStore } from 'redux';
 
 import createSagaMiddleware from 'redux-saga';
-import { fetchCollectionsStart } from './shop/shop.sagas';
 import logger from 'redux-logger';
 import { persistStore } from 'redux-persist';
 import rootReducer from './root-reducer';
+import rootSaga from "./root-saga";
 
 //* create saga
 const sagaMiddleware = createSagaMiddleware();
@@ -18,7 +18,7 @@ if (process.env.NODE_ENV === 'development') {
 
 const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-sagaMiddleware.run(fetchCollectionsStart);
+sagaMiddleware.run(rootSaga);
 
 //? REVIEW 在root-reducer里已经使用过persistReducer了，单独使用persistStore效果如何呢
 //? REVIEW 测试后发现如果没有persistReducer页面无法正常加载，persist/REHYDRATE触发失败
